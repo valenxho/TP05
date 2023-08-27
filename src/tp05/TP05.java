269613a760eba05413188b77127110ea1fb951a3
valenxho <142318159+valenxho@users.noreply.github.com>
2023-08-27 20:44:56 -0300

    Initial commit

---
+++ src/tp05/TP05.java
@@ -0,0 +1,17 @@
+
+package tp05;
+
+/**
+ *
+ * @author avoch
+ */
+public class TP05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
